ed2b239a4ef01ba5e16d71cd7d9c710490282f60
kevin <kevinke386@gmail.com>
2024-05-09 08:24:50 +0300

    Updated

--- src/CarDetails.jsx
+++ src/CarDetails.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const CarDetails = ({ car }) => {
+const CarDetails = ({ cars }) => {
   return (
-    <div className="car-details">
-      <h1>{`${car.make} ${car.model} ${car.year}`}</h1>
-      <img src={car.imageUrl} alt={`${car.make} ${car.model}`} />
-      <p>{car.description}</p>
-      <ul>
-        <li>Price: ${car.price}</li>
-        <li>Mileage: {car.mileage} kilometres</li>
-        
-      </ul>
-      <button>Test Drive</button>
+    <div>
+      {cars.map((car) => (
+        <div key={car.id} className="car-details">
+          <h1>{`${car.make} ${car.model} ${car.year}`}</h1>
+          <img src={car.imageUrl} alt={`${car.make} ${car.model}`} />
+          <p>{car.description}</p>
+          <ul>
+            <li>Price: ${car.price}</li>
+            <li>Colour: {car .colour}</li>
+          </ul>
+          <button>Test Drive</button>
+        </div>
+      ))}
     </div>
   );
 };
