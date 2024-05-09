a905c9352ed0964423355679998a941b288d30f1
Denleysworld <deekayel05@gmail.com>
2024-05-09 09:51:41 +0300

    updated the CarsListing code

--- src/assets/components/CarsListing.jsx
+++ src/assets/components/CarsListing.jsx
@@ -15,18 +15,17 @@ const CarsListing = () => {
   return (
     <div>
       <h1>Car Listing</h1>
-      {error && <p>{error}</p>}
       <ul>
         {cars.map(car => (
-          <li key={car.id}>
+          <div key={car.id}>
             <h2>{car.brand} {car.name}</h2>
-            <img src={car.image} alt={`${car.brand} ${car.name}`} />
+            <img src={car.picture} alt={`${car.brand} ${car.name}`} />
             <h2><p>{`${car.brand} ${car.name}`}</p></h2>
             <h2><strong><p>Price: ${car.price}</p></strong></h2>
             <p>Colour: {car.colour}</p>
             <p>Engine Type: {car.engineType}</p>
             <p>Condition: {car.condition}</p>
-          </li>
+          </div>
         ))}
       </ul>
     </div>
