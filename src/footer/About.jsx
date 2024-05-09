9b123edd4d51434f991e6affd173970ff27c5cae
Obala <oballasteve4@gmail.com>
2024-05-09 10:24:47 +0300

    adds the footer folder containing the footer elements

---
+++ src/footer/About.jsx
@@ -0,0 +1,13 @@
+// About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Road Runner Motors</h2>
+      {/* Add your about content here */}
+    </div>
+  );
+};
+
+export default About;
