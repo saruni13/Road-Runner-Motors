a8cf3216aa6ca565213b5c0747748a884d66036a
kevin <kevinke386@gmail.com>
2024-05-09 08:56:05 +0300

    Fetches data from db.json

--- src/CarDetails.jsx
+++ src/CarDetails.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const CarDetails = ({ cars }) => {
+const CarDetails = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch('db.json')
+      .then(response => response.json())
+      .then(data => setCars(data.Cars))
+      .catch(error => {
+        console.error('Error fetching cars:', error);
+      });
+  }, []); 
+
   return (
     <div>
       {cars.map((car) => (
         <div key={car.id} className="car-details">
-          <h1>{`${car.make} ${car.model} ${car.year}`}</h1>
-          <img src={car.imageUrl} alt={`${car.make} ${car.model}`} />
+          <h1>{`${car.brand} ${car.name} ${car.year}`}</h1>
+          <img src={car.picture} alt={`${car.brand} ${car.name}`} />
           <p>{car.description}</p>
           <ul>
             <li>Price: ${car.price}</li>
-            <li>Colour: {car .colour}</li>
+            <li>Colour: {car.colour}</li>
           </ul>
           <button>Test Drive</button>
         </div>
